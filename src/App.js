a3b329655dffedcd70c1380268603e856ce701b8
Mcgee, Eric <Eric.McGee@insight.com>
2018-11-28 05:09:17 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import TodoInput from './components/todoInput';
+import TodoItem from './components/todoItem';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {id: 0, text: "Learn Vue js !"},
+        {id: 1, text: "Play NBA2k19 on xboxlive"},
+        {id: 2, text: "Make a React app!"},
+        {id: 3, text: "Learn Angular 7 updates! "}
+      ],
+      nextId: 4
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todoText) {
+    let todos = this.state.todos.slice();
+    todos.push({id: this.state.nextId, text: todoText});
+    this.setState({
+      todos: todos,
+      nextId: ++this.state.nextId
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+        todos: this.state.todos.filter((todo, index) => todo.id !== id)
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="todo-wrapper">
+          <Header />
+          <TodoInput todoText="" addTodo={this.addTodo} />
+          <ul>
+            {
+              this.state.todos.map((todo) => {
+                return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo}/>
+              })
+            }
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+export default class Header extends React.Component{
+ render() {
+     return(
+     
+     <h1>Eric's React Todos</h1>
+     )
+     
+ }
+
+
+}
